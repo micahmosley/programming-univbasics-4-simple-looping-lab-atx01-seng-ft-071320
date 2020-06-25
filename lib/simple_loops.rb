4f34e8900fc19020cde6324a2f504859abde5db8
Micah Mosley <micah.mosley@utexas.edu>
2020-06-25 20:14:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,10 +16,10 @@ def loop_message_n_times(message, n)
   end
 end
 
-def output_array(message, n)
+def output_array(array)
   counter=0 
-  while counter<n
-  puts message
+  while counter<array.length
+  puts array[counter]
   counter+=1
   end
 end 
